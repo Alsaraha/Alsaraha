69d2184b931ba9e75c3bb1773cdf5cd052b47ce1
Alsaraha <abch8080@gmail.com>
2018-07-06 09:04:07 +0200

    adding sudo to mkdir

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 mkdir -p ~/.ejaaba
 cp ejaaba ~/bin/ejaaba
 echo 'Authentication for making man page directory'
-mkdir -p /usr/local/man/man1
+sudo mkdir -p /usr/local/man/man1
 echo 'Authentication for installing man page'
 sudo install -g 0 -o 0 -m 0644 ejaaba.1 /usr/local/man/man1/
 sudo gzip /usr/local/man/man1/ejaaba.1
